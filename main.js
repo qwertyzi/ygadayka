c8e6afd9d149b4d4983487bcc75c033386cafb6f
Artem <agaleev583@gmail.com>
2024-11-15 17:24:16 +0300

    feat:add loop for atempts

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 console.log("Я загадал число от 1 до 10. Попробуй угадать его!");
-let num = Number(prompt("Введите число:"));
 let secretNum = 8
-if (num < secretNum) {
-    console.log("Секретное число больше")
-} else if (num === secretNum) {
-    console.log("Вы угадали!!!!")
-} else {
-    console.log("The secret of monkey island")
+let attempt = 3
 
+while (attempt > 0) {
+    let num = Number(prompt("Введите число:"));
+    if (num < secretNum) {
+        console.log("Секретное число больше")
+        attempt--
+    } else if (num === secretNum) {
+        console.log("Вы угадали!!!!")
+        break
+    } else {
+        console.log("The secret of monkey island")
+        attempt--
+    }
 }
